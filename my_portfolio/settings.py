8d43ff22027a1cf5880ad8161e3195f459b301d4
suryaprakash1126 <nsuryaprakash31@gmail.com>
2025-06-27 17:27:17 +0530

    Add images and resume

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -116,12 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # collectstatic output
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # input folder
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
