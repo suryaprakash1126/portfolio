4b5e183325d59ed83b5d0274c55ab9cea2f6793d
suryaprakash1126 <nsuryaprakash31@gmail.com>
2025-06-27 17:19:31 +0530

    Fix static/media file paths and settings

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -117,8 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'portfolio_app' / 'static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
