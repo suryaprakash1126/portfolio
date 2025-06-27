3346cc0f654b4c1c8fb8502e4023aecbf61b411a
suryaprakash1126 <nsuryaprakash31@gmail.com>
2025-06-27 17:05:56 +0530

    url changes

--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('portfolio/', include('portfolio_app.urls') ),
+    path('', include('portfolio_app.urls') ),
 ]
