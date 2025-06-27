25699ef1bbf3d962598681f84632da72aee82081
suryaprakash1126 <nsuryaprakash31@gmail.com>
2025-06-27 17:52:34 +0530

    Fix: build command and static file handling

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -127,3 +127,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
